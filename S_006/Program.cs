2124dd4f66e55f85507d440ba544d2fdfef9a60c
PikichGleb <PikichGleb@yandex.ru>
2022-07-15 13:19:46 +0300

    HomeWork(S_006,S_012-S_015)

--- S_006/Program.cs
+++ S_006/Program.cs
@@ -1,3 +1,22 @@
 ﻿//6.Написать программу вычисления значения функции y = sin(a). (Класс Math)
 
-?
+//double x = 3.14;
+
+double x = Math.PI;
+double y = Math.Sin(x);
+double y1 = Math.Sin(x * 2);
+
+//Console.WriteLine(y);
+
+Console.WriteLine($"{y:F16}"); //F - фиксированнаое количество знаков после запятой
+
+Console.WriteLine(1.0 / 10000000.0);
+
+if (y == 0) Console.WriteLine("OK");
+else Console.WriteLine("Wrong");
+
+if (y <= 1E-10) Console.WriteLine("OK");
+else Console.WriteLine("Wrong");
+
+if (Math.Abs(y - y1) <= 1E-10) Console.WriteLine("OK");
+else Console.WriteLine("Wrong");
