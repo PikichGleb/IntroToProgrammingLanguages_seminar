aadb8ac788dbf61e00c59bc0ad2514c3c0006ab8
PikichGleb <PikichGleb@yandex.ru>
2022-08-10 08:15:41 +0300

    Seminar 5 HW (43, 46)

---
+++ S_043/Program.cs
@@ -0,0 +1,15 @@
+﻿// 43. С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+
+
+Console.ReadLine();
+string s = Console.ReadLine();
+string[] ss = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int[] a = Array.ConvertAll<string, int>(ss, int.Parse);
+int k = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] > 0)
+        k++;
+}
+Console.Write(k);
---
+++ S_046/Program.cs
@@ -0,0 +1,22 @@
+﻿// 46. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+int N =Convert.ToInt32(Console.ReadLine());
+int[] array= new int[N];
+
+array[0] = 0;
+array[1] = 1;
+
+for(int i =2;i<N;i++){
+    array[i] = array[i-1] + array[i-2];
+}
+
+Print(array);
+
+
+
+
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i],4}");
+}
