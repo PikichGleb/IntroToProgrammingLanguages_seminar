7766394c0130aefb25ab0f8d78ac70b9bdc50aba
PikichGleb <PikichGleb@yandex.ru>
2022-08-10 07:47:42 +0300

    Seminar 5

---
+++ S_e_judge/B/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+string s= Console.ReadLine();
+string[] ss=s.Split(' ');
+int a = int.Parse(ss[0]);
+int b = int.Parse(ss[1]);
+int c = int.Parse(ss[2]);
+
+Console.WriteLine(a+b+c);
---
+++ S_e_judge/One dimensional Array/001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вводится сначала число N, а затем N чисел. Выведите эти N чисел в обратном порядке.
+
+//int N =int.Parse(Console.ReadLine());
+using System;
+Console.ReadLine();
+string s = Console.ReadLine();
+string[] ss=s.Split(' ');
+for(int i =ss.Length-1;i>=0;i--)
+    Console.Write($"{ss[i]} ");
